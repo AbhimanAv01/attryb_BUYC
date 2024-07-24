4ad4126e3af7f7c0afd169be2d94c2fa7fe6d2a0
AbhimanAv01 <148443918+AbhimanAv01@users.noreply.github.com>
2024-07-24 13:05:29 +0530

    Update Footer.js

--- Frontend/src/Components/Footer/Footer.js
+++ Frontend/src/Components/Footer/Footer.js
@@ -1,25 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import "../../CSS/Footer.css";
-
-
-
-
-const Footer = () => {
-  const latitude = 12.9715987;  
-  const longitude = 77.5945627; 
-
-  // Create a custom red marker icon
-  const redMarkerIcon = new L.Icon({
-    iconUrl: 'https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers/img/marker-icon-red.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-   
-  });
 
   return (
     <footer className="footer-con">
@@ -63,23 +47,6 @@ const Footer = () => {
               <li>Phone: (123) 456-7890</li>
             </ul>
           </Col>
-          // <Col xs={12} md={5}>
-          //   <h5>Find Us</h5>
-          //   <MapContainer
-          //     center={[latitude, longitude]} // Set the map center to your desired coordinates
-          //     zoom={12}
-          //     style={{ height: '200px', width: '100%' }}
-          //   >
-          //     <TileLayer
-          //       url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          //     />
-          //     <Marker position={[latitude, longitude]} icon={redMarkerIcon}>
-          //       <Popup>
-          //         BUYC Corp <br /> 1234 Street Name, City, State, 56789.
-          //       </Popup>
-          //     </Marker>
-          //   </MapContainer>
-          // </Col>
         </Row>
         <hr className="my-4" />
         <Row>

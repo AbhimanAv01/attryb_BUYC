d297e4f17a3fa171d9231f01dcd40b948de0f8bd
AbhimanAv01 <148443918+AbhimanAv01@users.noreply.github.com>
2024-07-24 17:16:10 +0530

    Update Carousel.js

--- Frontend/src/Components/Carousel/Carousel.js
+++ Frontend/src/Components/Carousel/Carousel.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import img1 from '../../assets/slide1.png'
+import img1 from '../../assets/slide1.png';
+import img2 from '../../assets/slide2.png';
+import img3 from '../../assets/slide3.png';
 
 function DarkVariantExample() {
+  const imageStyle = {
+    height: '500px',  /
+    objectFit: 'cover', 
+    width: '100%', 
+  };
+
   return (
     <Carousel data-bs-theme="dark">
       <Carousel.Item>
@@ -9,35 +18,24 @@ function DarkVariantExample() {
           className="d-block w-100"
           src={img1}
           alt="First slide"
+          style={imageStyle} 
         />
-        {/* <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={img1}
+          src={img2}
           alt="Second slide"
+          style={imageStyle}  
         />
-        {/* <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption> */}
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={img1}
+          src={img3}
           alt="Third slide"
+          style={imageStyle}  
         />
-        {/* <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption> */}
       </Carousel.Item>
     </Carousel>
   );
